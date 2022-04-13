6cc1d5d623c76ab8410057a90481df1d3432fd28
Zubair <zubair.yousaf-eberl@gmx.de>
2022-04-13 12:07:18 +0200

    Herarchy-Bildung

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,6 +3,14 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
+
+        Child child = new Child();
+
+        
+        output(child.sayHello());
+        // output(child.sayHello("Hi!"));
+
+
      
     }
 
---
+++ com/cc/java/Parent.java
@@ -0,0 +1,12 @@
+package com.cc.java;
+
+public class Parent extends GrandParent {
+
+    // public String sayHello() {
+    //     return "Hello from Parent!";
+    // }
+    public String sayHello(String str) {
+        return str;
+    }
+
+}
